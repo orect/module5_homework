c3068f6692555671b650f4e950ed2445597c2663
orect <iamorest228@gmail.com>
2025-08-06 17:02:34 +0300

    Update exercice 2.py

--- exercice 2.py
+++ exercice 2.py
@@ -1,16 +1,14 @@
 import re
+
 def generator_numbers(text: str):
-
-    numbers = re.findall(r'\b\d+\.\d+\b', text)
-
+    
+    numbers = re.findall(r' (?P<num>\d+\.\d+) ', text)
     for number in numbers:
         yield float(number)
 
 def sum_profit(text: str, func):
-    numbers = func(text)
+    return sum(func(text))
 
-    total = sum(numbers)
-    return total
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
